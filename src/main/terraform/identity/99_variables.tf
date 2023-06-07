14f02da06669349f51abc8257db4c6e00589f542
anislucidi01 <anis.lucidi@pagopa.it>
2023-06-07 16:07:48 +0200

    Added files

---
+++ src/main/terraform/identity/99_variables.tf
@@ -0,0 +1,51 @@
+variable "prefix" {
+  type = string
+  validation {
+    condition = (
+      length(var.prefix) <= 6
+    )
+    error_message = "Max length is 6 chars."
+  }
+}
+
+variable "env" {
+  type        = string
+  description = "Environment"
+}
+
+variable "env_short" {
+  type = string
+  validation {
+    condition = (
+      length(var.env_short) <= 1
+    )
+    error_message = "Max length is 1 chars."
+  }
+}
+
+variable "github" {
+  type = object({
+    org        = string
+    repository = string
+  })
+  description = "GitHub Organization and repository name"
+  default = {
+    org        = "pagopa"
+    repository = "mil-preset"
+  }
+}
+
+variable "github_token" {
+  type        = string
+  sensitive   = true
+  description = "GitHub Organization and repository name"
+}
+
+variable "github_repository_environment_cd" {
+  type = object({
+    protected_branches     = bool
+    custom_branch_policies = bool
+    reviewers_teams        = list(string)
+  })
+  description = "GitHub Continous Integration roles"
+}
