8fb9e1428041c2f425de9ab358c3b30b31ff081a
anislucidi01 <anis.lucidi@pagopa.it>
2023-06-07 16:11:38 +0200

    added files for build automation

---
+++ src/main/terraform/identity/03_application_environment_cd.tf
@@ -0,0 +1,31 @@
+resource "azuread_application" "environment_cd" {
+  display_name = "${local.app_name}-cd"
+}
+
+resource "azuread_service_principal" "environment_cd" {
+  application_id = azuread_application.environment_cd.application_id
+}
+
+resource "azuread_application_federated_identity_credential" "environment_cd" {
+  application_object_id = azuread_application.environment_cd.object_id
+  display_name          = "github-federated"
+  description           = "github-federated"
+  audiences             = ["api://AzureADTokenExchange"]
+  issuer                = "https://token.actions.githubusercontent.com"
+  subject               = "repo:${var.github.org}/${var.github.repository}:environment:${var.env}-cd"
+}
+
+resource "azurerm_role_assignment" "environment_cd_subscription" {
+  scope                = data.azurerm_subscription.current.id
+  role_definition_name = "Contributor"
+  principal_id         = azuread_service_principal.environment_cd.object_id
+}
+
+output "azure_environment_cd" {
+  value = {
+    app_name       = "${local.app_name}-cd"
+    client_id      = azuread_service_principal.environment_cd.application_id
+    application_id = azuread_service_principal.environment_cd.application_id
+    object_id      = azuread_service_principal.environment_cd.object_id
+  }
+}
---
+++ src/main/terraform/identity/env/dev/terraform.tfvars
@@ -0,0 +1,11 @@
+prefix    = "mil"
+env_short = "d"
+env       = "dev"
+
+github_repository_environment_cd = {
+  protected_branches     = false
+  custom_branch_policies = true
+  reviewers_teams = [
+    "infrastructure-admins",
+    "mil-team",
+  ]
