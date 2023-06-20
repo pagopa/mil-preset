e9a90b9f7d08bae42f92f0508bbaa1aabfb2b7f2
d.sabatini <d.sabatini@accenture.com>
2023-06-20 16:44:08 +0200

    Fixed postman collection for preset, updated dep-sha256.json for Quarkus 3.1.1.Final

--- src/test/java/it/pagopa/swclient/mil/preset/it/PostmanCollectionTestIT.java
+++ src/test/java/it/pagopa/swclient/mil/preset/it/PostmanCollectionTestIT.java
@@ -1,30 +1,29 @@
 package it.pagopa.swclient.mil.preset.it;
 
-import java.time.Duration;
-import java.util.Optional;
-
+import io.quarkus.test.common.DevServicesContext;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+import io.quarkus.test.junit.TestProfile;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.runner.Description;
-import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.Testcontainers;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.containers.startupcheck.IndefiniteWaitOneShotStartupCheckStrategy;
-import org.testcontainers.containers.startupcheck.OneShotStartupCheckStrategy;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
+import org.testcontainers.utility.MountableFile;
 
-import io.quarkus.test.common.DevServicesContext;
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-import io.quarkus.test.junit.TestProfile;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Optional;
 
 @QuarkusIntegrationTest
 @TestProfile(IntegrationTestProfile.class)
@@ -45,16 +44,19 @@ class PostmanCollectionTestIT implements DevServicesContext.ContextAware {
     @BeforeAll
     void startNewmanContainer() {
 
-        //newmanContainer = new GenericContainer<>(DockerImageName.parse("postman/newman:alpine"))
         newmanContainer = new GenericContainer<>(DockerImageName.parse("dannydainton/htmlextra"))
-                //.withNetwork(getNetwork())
-                //.withNetworkMode(devServicesContext.containerNetworkId().get())
                 .waitingFor(Wait.forListeningPort())
                 .withStartupCheckStrategy(new IndefiniteWaitOneShotStartupCheckStrategy());
 
-        newmanContainer.withLogConsumer(new Slf4jLogConsumer(logger));
+        newmanContainer.withLogConsumer(new Slf4jLogConsumer(logger, true));
 
         String exposedPort = devServicesContext.devServicesProperties().get("test.wiremock.exposed-port");
+
+        try {
+            Files.createDirectories(Path.of("./target/newman-report"));
+        } catch (IOException e) {
+            logger.error("Error while create report directory", e);
+        }
 
         newmanContainer.setCommand(
                 "run",
@@ -62,11 +64,13 @@ class PostmanCollectionTestIT implements DevServicesContext.ContextAware {
                 "--disable-unicode",
                 "-k",
                 "--environment", "Local_IT.postman_environment.json",
-                "--env-var", "access_token_base_url=http://host.testcontainers.internal:" + exposedPort,
+                "--env-var", "MIL_IDP_BASE_URL=http://host.testcontainers.internal:" + exposedPort,
                 "--reporters", "htmlextra",
                 "--reporter-htmlextra-export", "reports/Preset_Service_Tests.html");
 
-        newmanContainer.withFileSystemBind("./src/test/postman", "/etc/newman");
+        newmanContainer.withFileSystemBind("./target/newman-report/", "/etc/newman/reports");
+        newmanContainer.withCopyToContainer(MountableFile.forHostPath(Path.of("./src/test/postman/Preset.postman_collection.json")), "/etc/newman/Preset.postman_collection.json");
+        newmanContainer.withCopyToContainer(MountableFile.forHostPath(Path.of("./src/test/postman/Local_IT.postman_environment.json")), "/etc/newman/Local_IT.postman_environment.json");
 
         // exposing port to newman container
         Config config = ConfigProvider.getConfig();
@@ -78,39 +82,31 @@ class PostmanCollectionTestIT implements DevServicesContext.ContextAware {
 
     }
 
+    @AfterAll
+    void stopNewmanContainer() {
+
+        if (newmanContainer != null) {
+            try {
+                newmanContainer.stop();
+            } catch (Exception e) {
+                logger.error("Error while stopping container", e);
+            }
+        }
+
+    }
+
+
     @Test
     void testPostmanCollection() {
 
         try {
             newmanContainer.start();
         } catch (Exception e) {
-            logger.error("Error while starting container", e);
+            logger.error("Error while starting container: {}", e.getMessage());
         }
 
         logger.info("newmanContainer.getCurrentContainerInfo().getState() -> {}", newmanContainer.getCurrentContainerInfo().getState());
         Assertions.assertEquals(0, newmanContainer.getCurrentContainerInfo().getState().getExitCodeLong());
-    }
-
-
-
-    private Network getNetwork() {
-        logger.info("devServicesContext.containerNetworkId() -> {}", devServicesContext.containerNetworkId());
-        return new Network() {
-            @Override
-            public String getId() {
-                return devServicesContext.containerNetworkId().get();
-            }
-
-            @Override
-            public void close() {
-
-            }
-
-            @Override
-            public Statement apply(Statement statement, Description description) {
-                return null;
-            }
-        };
     }
 
 }
