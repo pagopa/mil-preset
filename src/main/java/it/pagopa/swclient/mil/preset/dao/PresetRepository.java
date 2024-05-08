4c269ea8434090ce94795a58d8b820f0cd2ccf77
antoniotarricone <antonio.tarricone@pagopa.it>
2024-05-08 10:33:03 +0200

    Enabled OTel.

--- src/main/java/it/pagopa/swclient/mil/preset/dao/PresetRepository.java
+++ src/main/java/it/pagopa/swclient/mil/preset/dao/PresetRepository.java
@@ -1,12 +1,14 @@
 package it.pagopa.swclient.mil.preset.dao;
 
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepository;
+import it.pagopa.swclient.mil.observability.TraceReactivePanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 /**
  *  MongoDB repository to access the preset information, reactive flavor
  */
 @ApplicationScoped
+@TraceReactivePanacheMongoRepository
 public class PresetRepository implements ReactivePanacheMongoRepository<PresetEntity> {
 
 }
--- src/main/java/it/pagopa/swclient/mil/preset/dao/SubscriberRepository.java
+++ src/main/java/it/pagopa/swclient/mil/preset/dao/SubscriberRepository.java
@@ -1,12 +1,14 @@
 package it.pagopa.swclient.mil.preset.dao;
 
 import io.quarkus.mongodb.panache.reactive.ReactivePanacheMongoRepository;
+import it.pagopa.swclient.mil.observability.TraceReactivePanacheMongoRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 
 /**
  *  MongoDB repository to access the preset subscribed terminals, reactive flavor
  */
 @ApplicationScoped
+@TraceReactivePanacheMongoRepository
 public class SubscriberRepository implements ReactivePanacheMongoRepository<SubscriberEntity> {
 
 }
